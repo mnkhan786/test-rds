f6cc5bc00d2d009b52e3c6cff7e874534602f1da
mnkhan786 <mnadeemkhan@gmail.com>
2022-10-11 11:12:19 -0400

    My first commit

---
+++ rds.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "us-east-1"
+  access_key = "AKIAWNEQOMJ3I4IOQK4L"
+  secret_key = "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"
+  }
+  
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  name                 = "mydb"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+}
